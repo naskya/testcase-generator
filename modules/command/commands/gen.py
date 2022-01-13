283c055584dd40f29a769065cbf195beae50e905
naskya <m@naskya.net>
2022-01-14 00:13:34 +0900

    Use argparse module to parse command line arguments

--- modules/command/commands/gen.py
+++ modules/command/commands/gen.py
@@ -9,16 +9,15 @@ import psutil
 
 from modules.case.saver import save_case
 from modules.command.commands.impl.generate_one_case import generate_one_case
-from modules.command.definition import Command
 from modules.utility.colorizer import Color, colorize
 from modules.utility.exit_failure import exit_failure
 from modules.utility.printer import info, progress, progress_bar
 from modules.variable.definition import Variable
 
 
-def gen_with_progress_bar(command: Command, variables: dict[str, Variable],
+def gen_with_progress_bar(cases: int, prefix: str, suffix: str, verify: bool, variables: dict[str, Variable],
                           override_statements: str, format: list[list[str]]) -> None:
-    pad_length = len(str(command.cases)) + 1
+    pad_length = len(str(cases)) + 1
 
     progress('Start generating test cases.')
     print('-' * shutil.get_terminal_size().columns + '\n')
@@ -28,7 +27,7 @@ def gen_with_progress_bar(command: Command, variables: dict[str, Variable],
         test_number = 0
 
         futures = [executor.submit(generate_one_case, variables, override_statements, format)
-                   for _ in range(command.cases * 2)]
+                   for _ in range(cases * 2)]
 
         for future in concurrent.futures.as_completed(futures):
             success, result = future.result()
@@ -38,13 +37,13 @@ def gen_with_progress_bar(command: Command, variables: dict[str, Variable],
 
             test_number += 1
 
-            save_case(result, test_number, command.cases, command.prefix, command.suffix)
+            save_case(result, test_number, cases, prefix, suffix)
 
             # show progress
             print(f'\033[AGenerate #{str(test_number + 1).rjust(pad_length, " ")}')
-            progress_bar(test_number, command.cases, 50)
+            progress_bar(test_number, cases, 50)
 
-            if test_number == command.cases:
+            if test_number == cases:
                 print('\033[AGenerate: Done!\033[K\n\n')
                 executor.shutdown(wait=False)
 
@@ -58,25 +57,25 @@ def gen_with_progress_bar(command: Command, variables: dict[str, Variable],
                 break
 
     print('-' * shutil.get_terminal_size().columns)
-    if test_number != command.cases:
-        info(f'Failed to generate {command.cases - test_number} cases.')
+    if test_number != cases:
+        info(f'Failed to generate {cases - test_number} cases.')
 
     progress('{} (out of {}) cases have been generated successfully.'.format(
         colorize(Color.CODE, test_number),
-        colorize(Color.CODE, command.cases)
+        colorize(Color.CODE, cases)
     ))
 
-    if command.is_verification and test_number < command.cases // 2:
+    if verify and test_number < cases // 2:
         exit_failure()
 
 
-def gen_without_progress_bar(command: Command, variables: dict[str, Variable],
+def gen_without_progress_bar(cases: int, prefix: str, suffix: str, verify: bool, variables: dict[str, Variable],
                              override_statements: str, format: list[list[str]]) -> None:
     progress('Start generating test cases.')
 
     with concurrent.futures.ProcessPoolExecutor() as executor:
         test_number = 0
-        futures = [executor.submit(generate_one_case, variables, override_statements, format) for _ in range(command.cases * 2)]
+        futures = [executor.submit(generate_one_case, variables, override_statements, format) for _ in range(cases * 2)]
 
         for future in concurrent.futures.as_completed(futures):
             success, result = future.result()
@@ -86,9 +85,9 @@ def gen_without_progress_bar(command: Command, variables: dict[str, Variable],
 
             test_number += 1
 
-            save_case(result, test_number, command.cases, command.prefix, command.suffix)
+            save_case(result, test_number, cases, prefix, suffix)
 
-            if test_number == command.cases:
+            if test_number == cases:
                 executor.shutdown(wait=False)
 
                 try:
@@ -100,13 +99,13 @@ def gen_without_progress_bar(command: Command, variables: dict[str, Variable],
 
                 break
 
-    if test_number != command.cases:
-        info(f'Failed to generate {command.cases - test_number} cases.')
+    if test_number != cases:
+        info(f'Failed to generate {cases - test_number} cases.')
 
     progress('{} (out of {}) cases have been generated successfully.'.format(
         colorize(Color.CODE, test_number),
-        colorize(Color.CODE, command.cases)
+        colorize(Color.CODE, cases)
     ))
 
-    if command.is_verification and test_number < command.cases // 2:
+    if verify and test_number < cases // 2:
         exit_failure()
--- modules/command/commands/test_single.py
+++ modules/command/commands/test_single.py
@@ -7,7 +7,6 @@ from modules.case.saver import save_case_and_verdict_single
 from modules.command.commands.impl.definition import Result
 from modules.command.commands.impl.generate_one_case import generate_one_case
 from modules.command.commands.impl.test_one_case import test_one_case
-from modules.command.definition import Command
 from modules.utility.colorizer import Color, colorize
 from modules.utility.exit_failure import exit_failure
 from modules.utility.printer import info, progress, progress_bar
@@ -24,15 +23,16 @@ def generate_and_test_one_case(program: str, time_limit: int, variables: dict[st
     return True, test_case, test_one_case(program, test_case, time_limit)
 
 
-def test_single_with_progress_bar(command: Command, variables: dict[str, Variable],
+def test_single_with_progress_bar(cases: int, program_1: str, time_limit: float, prefix: str, suffix: str,
+                                  verify: bool, variables: dict[str, Variable],
                                   override_statements: str, format: list[list[str]]) -> None:
-    pad_length = len(str(command.cases)) + 1
+    pad_length = len(str(cases)) + 1
 
     progress('Start running tests.')
     print('-' * shutil.get_terminal_size().columns + '\n')
 
     print(f'Test #{"1".rjust(pad_length, " ")}')
-    progress_bar(0, command.cases, 50)
+    progress_bar(0, cases, 50)
     print()
     print()
 
@@ -40,8 +40,8 @@ def test_single_with_progress_bar(command: Command, variables: dict[str, Variabl
         try_number = 0
         test_number = 0
         detected_number = 0
-        futures = [executor.submit(generate_and_test_one_case, command.program_1, command.time_limit,
-                                   variables, override_statements, format) for _ in range(command.cases)]
+        futures = [executor.submit(generate_and_test_one_case, program_1, time_limit,
+                                   variables, override_statements, format) for _ in range(cases)]
 
         for future in concurrent.futures.as_completed(futures):
             try_number += 1
@@ -52,15 +52,14 @@ def test_single_with_progress_bar(command: Command, variables: dict[str, Variabl
 
                 if hasattr(test_result, 'exit_code') and (test_result.exit_code != 0):
                     test_result.verdict = 'RE'
-                elif test_result.time > command.time_limit:
+                elif test_result.time > time_limit:
                     test_result.verdict = 'TLE'
 
                 if test_result.verdict != '':
                     detected_number += 1
                     case_name, verdict_name = save_case_and_verdict_single(test_case, detected_number,
-                                                                           command.cases, command.prefix,
-                                                                           command.suffix, command.time_limit,
-                                                                           command.program_1, test_result)
+                                                                           cases, prefix, suffix, time_limit,
+                                                                           program_1, test_result)
 
                     print('Test #{}: {} --> saved as {} and {}'.format(
                         str(test_number).rjust(pad_length, ' '),
@@ -71,9 +70,8 @@ def test_single_with_progress_bar(command: Command, variables: dict[str, Variabl
                 elif not hasattr(test_result, 'stdout'):
                     detected_number += 1
                     case_name, verdict_name = save_case_and_verdict_single(test_case, detected_number,
-                                                                           command.cases, command.prefix,
-                                                                           command.suffix, command.time_limit,
-                                                                           command.program_1, test_result)
+                                                                           cases, prefix, suffix, time_limit,
+                                                                           program_1, test_result)
                     print('Test #{}: Failed to capture the output --> saved as {} and {} (maybe not your fault! just in case.)'.format(
                         str(test_number).rjust(pad_length, ' '),
                         colorize(Color.CODE, case_name),
@@ -82,42 +80,43 @@ def test_single_with_progress_bar(command: Command, variables: dict[str, Variabl
 
             print('\033[A' * (detected_number + 3), end='')
 
-            if try_number == command.cases:
+            if try_number == cases:
                 print('Test: Done!\033[K')
-                progress_bar(try_number, command.cases, 50)
+                progress_bar(try_number, cases, 50)
 
                 for _ in range(2 if (detected_number == 0) else detected_number + 3):
                     print()
             else:
                 print(f'Test #{str(test_number + 1).rjust(pad_length, " ")}')
-                progress_bar(try_number, command.cases, 50)
+                progress_bar(try_number, cases, 50)
 
                 for _ in range(detected_number + 2):
                     print()
 
     print('-' * shutil.get_terminal_size().columns)
-    if test_number != command.cases:
-        info(f'Failed to generate {colorize(Color.CODE, command.cases - test_number)} cases.')
+    if test_number != cases:
+        info(f'Failed to generate {colorize(Color.CODE, cases - test_number)} cases.')
 
     progress('{} (out of {}) tests run successfully.'.format(
         colorize(Color.CODE, test_number),
-        colorize(Color.CODE, command.cases)
+        colorize(Color.CODE, cases)
     ))
 
-    if command.is_verification and test_number < command.cases // 2:
+    if verify and test_number < cases // 2:
         exit_failure()
 
 
-def test_single_without_progress_bar(command: Command, variables: dict[str, Variable],
+def test_single_without_progress_bar(cases: int, program_1: str, time_limit: float, prefix: str, suffix: str,
+                                     verify: bool, variables: dict[str, Variable],
                                      override_statements: str, format: list[list[str]]) -> None:
-    pad_length = len(str(command.cases)) + 1
+    pad_length = len(str(cases)) + 1
     progress('Start running tests.\n')
 
     with concurrent.futures.ProcessPoolExecutor() as executor:
         test_number = 0
         detected_number = 0
-        futures = [executor.submit(generate_and_test_one_case, command.program_1, command.time_limit,
-                                   variables, override_statements, format) for _ in range(command.cases)]
+        futures = [executor.submit(generate_and_test_one_case, program_1, time_limit,
+                                   variables, override_statements, format) for _ in range(cases)]
 
         for future in concurrent.futures.as_completed(futures):
             success, test_case, test_result = future.result()
@@ -127,15 +126,14 @@ def test_single_without_progress_bar(command: Command, variables: dict[str, Vari
 
                 if hasattr(test_result, 'exit_code') and (test_result.exit_code != 0):
                     test_result.verdict = 'RE'
-                elif test_result.time > command.time_limit:
+                elif test_result.time > time_limit:
                     test_result.verdict = 'TLE'
 
                 if test_result.verdict != '':
                     detected_number += 1
                     case_name, verdict_name = save_case_and_verdict_single(test_case, detected_number,
-                                                                           command.cases, command.prefix,
-                                                                           command.suffix, command.time_limit,
-                                                                           command.program_1, test_result)
+                                                                           cases, prefix, suffix, time_limit,
+                                                                           program_1, test_result)
 
                     print('Test #{}: {} --> saved as {} and {}'.format(
                         str(test_number).rjust(pad_length, ' '),
@@ -146,9 +144,8 @@ def test_single_without_progress_bar(command: Command, variables: dict[str, Vari
                 elif not hasattr(test_result, 'stdout'):
                     detected_number += 1
                     case_name, verdict_name = save_case_and_verdict_single(test_case, detected_number,
-                                                                           command.cases, command.prefix,
-                                                                           command.suffix, command.time_limit,
-                                                                           command.program_1, test_result)
+                                                                           cases, prefix, suffix, time_limit,
+                                                                           program_1, test_result)
                     print('Test #{}: Failed to capture the output --> saved as {} and {} (maybe not your fault! just in case.)'.format(
                         str(test_number).rjust(pad_length, ' '),
                         colorize(Color.CODE, case_name),
@@ -158,13 +155,13 @@ def test_single_without_progress_bar(command: Command, variables: dict[str, Vari
         if detected_number > 0:
             print()
 
-    if test_number != command.cases:
-        info(f'Failed to generate {colorize(Color.CODE, command.cases - test_number)} cases.')
+    if test_number != cases:
+        info(f'Failed to generate {colorize(Color.CODE, cases - test_number)} cases.')
 
     progress('{} (out of {}) tests run successfully.'.format(
         colorize(Color.CODE, test_number),
-        colorize(Color.CODE, command.cases)
+        colorize(Color.CODE, cases)
     ))
 
-    if command.is_verification and test_number < command.cases // 2:
+    if verify and test_number < cases // 2:
         exit_failure()
